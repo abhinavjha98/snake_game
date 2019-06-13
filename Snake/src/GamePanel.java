3faab7e3906a313d63654085c1732fbe5ae551a4
abhinavjha98 <abhinavjha98ald@gmail.com>
2019-06-13 12:18:27 +0530

    Added the folder

---
+++ Snake/src/GamePanel.java
@@ -0,0 +1,32 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements Runnable, KeyListener {
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
